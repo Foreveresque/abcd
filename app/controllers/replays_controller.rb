ba92df6f6a4fee3650407cd7c3fc89ee78e35ebb
unknown <p14@.(none)>
2012-04-23 00:14:00 +0200

    init

---
+++ app/controllers/replays_controller.rb
@@ -0,0 +1,13 @@
+class ReplaysController < ApplicationController
+  def index
+  end
+
+  def view
+  end
+
+  def edit
+  end
+
+  def create
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myapp::Application.config.secret_token = 'cea707d1e542ae3e893b29ecb7a8b1b3fa93bdcf820e2514dcf0f26a3270197f8349d13250268b4d64406efeca9d04a88210f5fe140c1b533c1ef039aaea725d'
