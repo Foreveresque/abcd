f19adaaa0bb8f5e49024efb83fbb5a98c590d0b9
Senchantorpeist <p14@Luppy.(none)>
2014-04-06 22:32:37 +0200

    precompile

--- Gemfile
+++ Gemfile
@@ -29,7 +29,7 @@ gem 'yaml_db', github: 'jetthoughts/yaml_db'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem "sass", "~> 3.2.5"
+  gem "sass", "~> 3.2.13"
   gem 'sass-rails'
   gem 'coffee-rails', '~> 3.2.1'
 
--- config/application.rb
+++ config/application.rb
@@ -52,10 +52,8 @@ module Myapp
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.compress = false
-    config.assets.debug = true
     
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
