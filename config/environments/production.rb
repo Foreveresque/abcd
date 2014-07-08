d851bf8f90d586dad96b26ddec2f4f4a3875a822
Senchantorpeist <p14@Luppy.(none)>
2014-07-09 00:57:09 +0200

    production precompile1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,8 +19,6 @@ Myapp::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
-  
-  config.assets.precompile += %w( jquery.tools.min.js)
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
@@ -49,6 +47,8 @@ Myapp::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  
+  config.assets.precompile += ['jquery.tools.min.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
