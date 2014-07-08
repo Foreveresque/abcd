1a32094a2808d1d8e91f68649e17d9f7a291b682
Senchantorpeist <p14@Luppy.(none)>
2014-07-09 01:37:03 +0200

    config assets false, testing precompile

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module Myapp
     # Enable the asset pipeline
     config.assets.enabled = true
     
-    config.assets.precompile += %w( jquery.tools.min.js application.js )
+    config.assets.precompile += [ '*.js' '*.css' ] 
     
     config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Myapp::Application.initialize!
+
+config.assets.precompile += %w( jquery.tools.min.js )
